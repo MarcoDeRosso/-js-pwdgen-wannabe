461884463dad86fc8660af6909e5a7664a9878a8
MarcoDeRosso <mcoder@hotmail.it>
2021-05-27 13:53:39 +0200

    generator ok

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var firstname = prompt("Inserisci Nome");
+
+var surname = prompt("Inserisci Cognome");
+
+var color = prompt("Inserisci colore preferito");
+
+var result = firstname + surname + color;
+
+document.getElementById("password").innerHTML = result + 21;
